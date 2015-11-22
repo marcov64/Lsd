91acf3b35a6e873ea9bfe4d026f94a9dea3da55c
marcov64 <valente.marco@gmail.com>
2015-11-22 17:02:13 +0100

    Fixing post-error crash
    After errors due to missing elements the system crashed. The fix skips
    some try{} catch{} commands and takes care of resetting the stacklog
    only when necessary. Best would be to avoid the try to the run() in the
    first place.

--- src/lsdmain.cpp
+++ src/lsdmain.cpp
@@ -534,7 +534,11 @@ run(root);
 /**/
 catch(int p)
  {
- cmd(inter, "puts here");
+ //cmd(inter, "puts here");
+ while(stacklog->prev!=NULL)
+   stacklog=stacklog->prev;
+ stack=0; 
+ 
  }
 /**/ 
 cmd(inter, "wm deiconify .");
