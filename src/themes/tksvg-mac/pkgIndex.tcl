36aa39745a7cbc964f9e1654839010ce03d5e061
Marcelo Pereira <marcelocpereira@uol.com.br>
2021-04-04 21:37:57 -0300

    Fix tksvg in Linux & macOS

--- src/themes/tksvg-mac/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded tksvg 0.7 \
-    [list load [file join $dir libtksvg0.7.dylib] tksvg]
--- src/themes/tksvg-windows/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded tksvg 0.7 \
-    [list load [file join $dir tksvg07t.dll] tksvg]
