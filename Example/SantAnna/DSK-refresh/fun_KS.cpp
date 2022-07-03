1a15de13abaab76f2bd28a7ea0ab95578377466a
Marcelo Pereira <marcelocpereira@uol.com.br>
2022-07-03 18:49:25 -0300

    Rename K+S climate example to DSK Refresh

--- Example/SantAnna/DSK-refresh/fun_KS.cpp
+++ Example/SantAnna/DSK-refresh/fun_KS.cpp
@@ -1,14 +1,14 @@
 /******************************************************************************
 
-	K+S LSD MODEL (including finance, variable firms, climate and energy)
-	-------------
+	DSK REFRESH (CLIMATE-AUGMENTED K+S) LSD MODEL
+	---------------------------------------------
 
 	Written by Marcelo C. Pereira, University of Campinas
 
 	Copyright Marcelo C. Pereira
 	Distributed under the GNU General Public License
 
-	VERSION: 0.4.1 - climate model and energy sector
+	VERSION: 0.4.1 - climate model
 
 	This is the topmost code file for the K+S coded in LSD.
 	It contains only the scheduling equations 'runCountry' and 'timeStep',
