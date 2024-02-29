9f93636398f552bb09ee63c943a8c85ecae2a6eb
Marcelo Pereira <mcp1@users.noreply.github.com>
2024-02-29 17:37:05 -0300

    Update Windows MSYS/Cygwin

--- gnu/include/glib-2.0/gobject/glib-enumtypes.h
+++ gnu/include/glib-2.0/gobject/glib-enumtypes.h
@@ -8,7 +8,7 @@
 
 G_BEGIN_DECLS
 
-/* enumerations from "../glib-2.78.3/gobject/../glib/gunicode.h" */
+/* enumerations from "../glib-2.78.4/gobject/../glib/gunicode.h" */
 GOBJECT_AVAILABLE_IN_2_60 GType g_unicode_type_get_type (void) G_GNUC_CONST;
 #define G_TYPE_UNICODE_TYPE (g_unicode_type_get_type ())
 GOBJECT_AVAILABLE_IN_2_60 GType g_unicode_break_type_get_type (void) G_GNUC_CONST;
--- gnu/include/pixman-1/pixman-version.h
+++ gnu/include/pixman-1/pixman-version.h
@@ -33,9 +33,9 @@
 
 #define PIXMAN_VERSION_MAJOR 0
 #define PIXMAN_VERSION_MINOR 43
-#define PIXMAN_VERSION_MICRO 0
+#define PIXMAN_VERSION_MICRO 2
 
-#define PIXMAN_VERSION_STRING "0.43.0"
+#define PIXMAN_VERSION_STRING "0.43.2"
 
 #define PIXMAN_VERSION_ENCODE(major, minor, micro) (	\
 	  ((major) * 10000)				\
